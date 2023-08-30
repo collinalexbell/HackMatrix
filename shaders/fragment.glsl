e03ba7a2b4abfc0f7209c0b2fb8d4ed5c78130fe
kuberlog <collinalexbell@gmail.com>
2023-08-30 21:09:30 +0000

    prepped to do the smile texture

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,9 +4,11 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-  FragColor = texture(ourTexture, TexCoord);
+  FragColor = mix(texture(texture1, TexCoord),
+                  texture(texture2, TexCoord), 0.2);
 }
