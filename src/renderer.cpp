4c0cdfc987b474f7b2556c3cc9637a62dcf9fbbf
kuberlog <collinalexbell@gmail.com>
2023-09-06 08:30:15 +0000

    add glm

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,6 +5,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 float vertices[] = {
   // positions // colors // texture coords
