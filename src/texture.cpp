4169391488936ecbc802114f1dc4c28455fda7b0
kuberlog <collinalexbell@gmail.com>
2023-09-06 06:58:15 +0000

    refactor texture

--- src/texture.cpp
+++ src/texture.cpp
@@ -6,30 +6,22 @@
 #include <glad/glad.h>
 #include <iostream>
 
-
-Texture::Texture(std::string fname, GLenum unit) {
+GLenum textureFormat(std::string fname) {
   std::string ext = fname.substr(fname.find_last_of(".") + 1);
-  GLenum textureFormat;
+  GLenum rv;
   if(ext == "png") {
-    textureFormat = GL_RGBA;
+    rv = GL_RGBA;
   } else {
-    textureFormat = GL_RGB;
+    rv = GL_RGB;
   }
-  glGenTextures(1, &ID);
-  // need to provide texture binding for multi-texture work
-  glActiveTexture(unit);
-  glBindTexture(GL_TEXTURE_2D, ID);
-  // set the texture wrapping/filtering options (on currently bound texture)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  return rv;
+}
 
-  stbi_set_flip_vertically_on_load(true);
-  data = stbi_load(fname.c_str(), &width, &height, &nrChannels, 0);
+void Texture::loadTextureData(std::string fname) {
+  unsigned char* data = stbi_load(fname.c_str(), &width, &height, &nrChannels, 0);
   if (data)
     {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, textureFormat,
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, textureFormat(fname),
                    GL_UNSIGNED_BYTE, data);
       glGenerateMipmap(GL_TEXTURE_2D);
     }
@@ -38,4 +30,24 @@ Texture::Texture(std::string fname, GLenum unit) {
       std::cout << "Failed to load texture" << std::endl;
     }
   stbi_image_free(data);
+}
+
+void Texture::initAndBindGlTexture(GLenum unit) {
+  glGenTextures(1, &ID);
+  glActiveTexture(unit);
+  glBindTexture(GL_TEXTURE_2D, ID);
+}
+
+void setTextureParameters() {
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+Texture::Texture(std::string fname, GLenum unit) {
+  initAndBindGlTexture(unit);
+  setTextureParameters();
+  stbi_set_flip_vertically_on_load(true);
+  loadTextureData(fname);
 }
