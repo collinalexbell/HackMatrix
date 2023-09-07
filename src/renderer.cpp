31aa9b0931b44623de9ff7122a919d029c4a7df0
kuberlog <collinalexbell@gmail.com>
2023-09-07 20:08:12 +0000

    translate and rotate

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -75,6 +75,7 @@ Renderer::Renderer() {
 void Renderer::render() {
   angle = angle + 1;
   trans = glm::mat4(1.0f);
+  trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
   trans = glm::rotate(trans, glm::radians(angle), glm::vec3(0.0, 0.0, 1.0));
   trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
   glClear(GL_COLOR_BUFFER_BIT);
