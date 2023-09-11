e41f558f242e6faf4eb2b376081dea2d1568e8c8
kuberlog <collinalexbell@gmail.com>
2023-09-11 06:02:46 +0000

    many cubes! lets fucking go!!!

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -125,13 +125,35 @@ void Renderer::updateTransformMatrices() {
   glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
+glm::vec3 cubePositions[] = {
+  glm::vec3( 0.0f, 0.0f, 0.0f),
+  glm::vec3( 2.0f, 5.0f, -15.0f),
+  glm::vec3(-1.5f, -2.2f, -2.5f),
+  glm::vec3(-3.8f, -2.0f, -12.3f),
+  glm::vec3( 2.4f, -0.4f, -3.5f),
+  glm::vec3(-1.7f, 3.0f, -7.5f),
+  glm::vec3( 1.3f, -2.0f, -2.5f),
+  glm::vec3( 1.5f, 2.0f, -2.5f),
+  glm::vec3( 1.5f, 0.2f, -1.5f),
+  glm::vec3(-1.3f, 1.0f, -1.5f)
+};
+
 void Renderer::render() {
-  computeTransform();
+  //computeTransform();
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glBindTexture(GL_TEXTURE_2D, textures["container"]->ID);
   glBindTexture(GL_TEXTURE_2D, textures["face"]->ID);
   updateTransformMatrices();
-  glDrawArrays(GL_TRIANGLES, 0, 36);
+  for (unsigned int i = 0; i < 10; i++){
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[i]);
+    float angle = 20.0f * i;
+    model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+    unsigned int modelLoc = glGetUniformLocation(shader->ID,"model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+  }
 }
 
 Renderer::~Renderer() {
