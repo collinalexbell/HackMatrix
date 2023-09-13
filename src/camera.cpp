abd476569c95b68fd3016e700931d37625531a6a
kuberlog <collinalexbell@gmail.com>
2023-09-13 19:26:40 +0000

    ngmi

--- src/camera.cpp
+++ src/camera.cpp
@@ -59,7 +59,7 @@ void Camera::handleRotateForce(GLFWwindow* window, double xpos, double ypos) {
   front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
   front.y = sin(glm::radians(pitch));
   front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-  this->cameraFrontfront = glm::normalize(front);
+  this->front = glm::normalize(front);
 }
 
 glm::mat4 Camera::getViewMatrix() {
