355f8865083b55939f31ef810d253395fe8fc4ba
kuberlog <collinalexbell@gmail.com>
2023-09-26 19:43:51 +0000

    using blocks

--- src/world.cpp
+++ src/world.cpp
@@ -1,18 +1,29 @@
 #include "world.h"
+#include "renderer.h"
 #include <vector>
 #include <glm/glm.hpp>
+#include <iostream>
+
+using namespace std;
 
 
 
-World::World(){}
+World::World(){
+  Cube nullCube;
+  nullCube.blockType = -1;
+  cubes = std::vector<Cube>(CHUNK_SIZE*CHUNK_SIZE*CHUNK_SIZE, nullCube);
+}
 World::~World() {}
 
 const std::vector<Cube> World::getCubes() {
-  std::vector<Cube> rv(cubes.size());
-  for(auto kv: cubes) {
-    Cube cube = kv.second;
-    rv[cube.order] = cube;
+  cout << "getting cubes" << endl;
+  std::vector<Cube> rv(cubeCount);
+  for(auto cube: cubes) {
+    if(cube.blockType != -1) {
+      rv[cube.order] = cube;
+    }
   }
+  cout << "returning cubes" << endl;
   return rv;
 }
 const std::vector<glm::vec3> World::getAppCubes() {
@@ -23,12 +34,14 @@ const std::vector<glm::vec3> World::getAppCubes() {
   return appCubeKeys;
 }
 
-void World::addCube(glm::vec3 pos, int blockType) {
-  int index = cubes.size();
-  Cube cube{pos, blockType, index};
-  cubes.insert(std::pair<glm::vec3, Cube>(pos, cube));
+void World::addCube(int x, int y, int z, int blockType) {
+  int index = x * (CHUNK_SIZE*CHUNK_SIZE) + y * (CHUNK_SIZE) + z;
+  int orderIndex = cubeCount++;
+  glm::vec3 pos(x, y, z);
+  Cube cube{pos, blockType, orderIndex};
+  cubes[index] = cube;
   if(renderer != NULL) {
-    renderer->addCube(index);
+    renderer->addCube(orderIndex, cubes[index]);
   }
 }
 
@@ -45,6 +58,14 @@ void World::attachRenderer(Renderer* renderer){
 }
 
 Cube* World::getVoxel(float x, float y, float z) {
-  // I'll probably need to store voxels using integers so then I can index into them easily
+  int index = x * (CHUNK_SIZE*CHUNK_SIZE) + y * (CHUNK_SIZE) + z;
+  Cube* rv = &cubes[index];
+  if(rv->blockType != -1) {
+    return rv;
+  }
   return NULL;
+}
+
+int World::size() {
+  return cubeCount;
 }
