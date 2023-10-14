fa2be94ba838a6139de00124d2d904efe516b403
kuberlog <collinalexbell@gmail.com>
2023-10-14 23:24:25 +0000

    can open app by class now

--- include/app.h
+++ include/app.h
@@ -10,6 +10,11 @@
 
 using namespace std;
 
+enum IdentifierType {
+  NAME,
+  CLASS
+};
+
 class X11App {
   Display* display;
   int screen;
@@ -17,16 +22,19 @@ class X11App {
   XWindowAttributes attrs;
   GLXFBConfig* fbConfigs;
   int fbConfigCount;
-  void fetchInfo(string windowName);
+  void fetchInfo(string windowName, IdentifierType identifierType);
   int textureUnit = -1;
   int textureId = -1;
   atomic_bool focused = false;
+  X11App(Display *display, int screen);
 
 public:
+  static X11App *byName(string windowName, Display *display, int screen,
+                        int width, int height);
+  static X11App *byClass(string windowClass, Display *display, int screen,
+                         int width, int height);
   int width;
   int height;
-  X11App(string windowName, Display *display, int screen, int width,
-         int height);
   void appTexture();
   void attachTexture(int textureUnit, int textureId);
   void click(int x, int y);
