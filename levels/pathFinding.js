5910350517747eb8342dafed84e139b30e99a304
kuberlog <collinalexbell@gmail.com>
2023-10-17 04:31:35 +0000

    untested, but pretty much copy of demo code

--- levels/pathFinding.js
+++ levels/pathFinding.js
@@ -1,1 +1,19 @@
 // Write a path finder
+const protobuf = require("protobufjs");
+const zmq = require("zeromq");
+const root = protobuf.loadSync("include/protos/api.proto");
+const AddCube = root.lookupType("AddCube");
+
+async function runRequester() {
+    const socket = new zmq.Request();
+
+    await socket.connect("tcp://127.0.0.1:5555");
+
+    const request = AddCube.encode({ x:4, y:4, z:4, blockType:0 }).finish();
+
+    // Send the request to the server
+    socket.send(request);
+    const [result] = await socket.receive();
+}
+
+runRequester();
