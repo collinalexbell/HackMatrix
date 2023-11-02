e9a860cf55bb63dc1c17b85a304d3f334b25abb4
kuberlog <collinalexbell@gmail.com>
2023-11-02 01:51:57 +0000

    using external vectors

--- include/cube.h
+++ include/cube.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#include <vector>
 #include <glm/glm.hpp>
+using namespace std;
 
 class Cube {
-  glm::vec3 _position = glm::vec3(0,0,0);
-  int _blockType;
-  int _selected = 0;
+  static vector<glm::vec3> vecs;
+  static vector<int> ints;
+  int index;
 
 public:
   Cube();
+  Cube(const Cube &cpy);
   Cube(glm::vec3 position, int blockType);
   Cube(glm::vec3 position, int blockType, int selected);
+  Cube &operator=(const Cube &other);
   bool operator==(const Cube &cmp);
   glm::vec3 position() const;
   glm::vec3 &position();
