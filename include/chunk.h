5037e07c18d3356d67c5916935bdbbac88205f9d
kuberlog <collinalexbell@gmail.com>
2023-11-05 20:01:07 +0000

    unique_ptr for chunk data

--- include/chunk.h
+++ include/chunk.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "cube.h"
+#include <memory>
 
 class Chunk {
-  Cube* data[128*128*128];
+  static const int xSize = 256;
+  static const int ySize = 256;
+  static const int zSize = 128;
+  unique_ptr<Cube* []> data;
   Cube null = Cube(glm::vec3(0, 0, 0), -1);
 
   int index(int x, int y, int z);
