aa94cbba501d22227a5ae3550cc2ef75ecd21090
kuberlog <collinalexbell@gmail.com>
2023-11-06 02:09:49 +0000

    fix dimensions and get rid of CHUNK_SIZE

--- include/chunk.h
+++ include/chunk.h
@@ -1,14 +1,12 @@
 #pragma once
 #include "cube.h"
 #include <memory>
+#include <vector>
 
 class Chunk {
-  static const int xSize = 256;
-  static const int ySize = 256;
-  static const int zSize = 128;
+  const vector<int> size = {256, 128, 256};
   unique_ptr<Cube* []> data;
   Cube null = Cube(glm::vec3(0, 0, 0), -1);
-
   int index(int x, int y, int z);
 
 public:
@@ -16,4 +14,5 @@ public:
   Cube *getCube(int x, int y, int z);
   void removeCube(int x, int y, int z);
   void addCube(Cube c, int x, int y, int z);
+  const vector<int> getSize();
 };
