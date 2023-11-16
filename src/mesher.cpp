1b003e1ac9bb720744c3ff8eae63fe8c5c8a38ed
kuberlog <collinalexbell@gmail.com>
2023-11-16 17:39:53 +0000

    mask

--- src/mesher.cpp
+++ src/mesher.cpp
@@ -2,6 +2,17 @@
 
 Mesh Mesher::mesh(Chunk* chunk) {
   for (int dimension = 0; dimension < 3; ++dimension) {
+    int i, j, k, l, w, h;
+    int u = (dimension + 1) % 3;
+    int v = (dimension + 2) % 3;
+    int *x = new int[3];
+    int *q = new int[3];
+
+    vector<int> chunkSizes = chunk->getSize();
+
+    bool *mask = new bool[chunkSizes[0]*chunkSizes[1]*chunkSizes[2]];
   }
+
+
   return Mesh{};
 }
