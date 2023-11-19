3cfb414270739741f3a18e10cc974d43e3f98fab
kuberlog <collinalexbell@gmail.com>
2023-11-19 06:07:48 +0000

    the mesher works ( i think ), but it is REALLY slow. Will probably need to mesh things that are static and directly render things that aren't

--- include/mesher.h
+++ include/mesher.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "chunk.h"
+#include <memory>
+#include "logger.h"
 
 typedef vector<glm::vec3> Mesh;
 
 class Mesher {
- public:
-  Mesher() {}
-  Mesh mesh(Chunk* chunk);
+  std::shared_ptr<spdlog::logger> logger;
+public:
+  Mesher();
+  Mesh mesh(Chunk *chunk);
 };
