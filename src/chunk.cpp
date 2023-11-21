d6eded5a006e1fca78689f32558b1c83912776a4
kuberlog <collinalexbell@gmail.com>
2023-11-21 06:37:43 +0000

    made a lot of progress here... still don't know how I'm getting vertices to GPU though

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -29,15 +29,45 @@ void Chunk::addCube(Cube c, int x, int y, int z) {
 
 ChunkMesh Chunk::mesh() {
   ChunkMesh rv;
+  int x,y,z;
   int totalSize = size[0] * size[1] * size[2];
+  ChunkCoords neighborCoords;
+  Cube* neighbor;
   for(int i = 0; i<totalSize; i++) {
     if(data[i] != NULL) {
+      ChunkCoords ci = getCoords(i);
+      ChunkCoords neighbors[6] = {
+        ChunkCoords{ci.x - 1, ci.y, ci.z},
+        ChunkCoords{ci.x + 1, ci.y, ci.z},
+        ChunkCoords{ci.x, ci.y - 1, ci.z},
+        ChunkCoords{ci.x, ci.y + 1, ci.z},
+        ChunkCoords{ci.x, ci.y, ci.z - 1},
+        ChunkCoords{ci.x, ci.y, ci.z + 1}};
+
+      for(int neighborIndex = 0; neighborIndex < 6; neighborIndex++) {
+        neighborCoords = neighbors[neighborIndex];
+        neighbor = getCube(neighborCoords.x, neighborCoords.y, neighborCoords.z);
+      }
     }
   }
   return rv;
 }
 
-int Chunk::index(int x, int y, int z) { return x * size[1] * size[2] + y * size[2] + z; }
+int Chunk::index(int x, int y, int z) {
+  return x * size[1] * size[2] + y * size[2] + z;
+}
+
+ChunkCoords Chunk::getCoords(int index) {
+  ChunkCoords rv;
+  rv.z = index % size[2];
+  index /= size[2];
+
+  rv.y = index % size[1];
+  index /= size[1];
+
+  rv.x = index;
+  return rv;
+}
 
 const vector<int> Chunk::getSize() {
   return size;
