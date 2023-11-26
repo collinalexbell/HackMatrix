d3f6776a238bf3b82d873b76a776180d3d2d3491
kuberlog <collinalexbell@gmail.com>
2023-11-26 00:46:09 +0000

    mappilary access works!

--- scraper/mappilary.py
+++ scraper/mappilary.py
@@ -1,44 +1,26 @@
 import requests, json
+import yaml
 
-# Define the necessary details
-client_id = 'your_client_id'
-client_secret = 'your_client_secret'
-token_url = 'https://example.com/oauth/token'  # Replace with your token URL
 
-token_params = {
-    'grant_type': 'client_credentials',  # Adjust based on the OAuth flow you are using
-    'client_id': client_id,
-    'client_secret': client_secret
-}
+# Load credentials from creds.yaml
+with open('creds.yaml', 'r') as file:
+    creds = yaml.safe_load(file)
 
-# Other parameters required by the token endpoint
-response = requests.post(token_url, data=token_params)
+# Extract credentials from the loaded data
+access_token = creds.get('access_token')
 
-# Check if the request was successful
+# Mapillary access token -- provide your own, replace this example
+mly_key = access_token
+
+seq = 'kx7r2uYeFwT6XCfvqh30VZ'
+
+url = f'https://graph.mapillary.com/image_ids?access_token={mly_key}&sequence_id={seq}'
+
+response = requests.get(url)
+
+image_ids = None
+
 if response.status_code == 200:
-    # Access token received
-    access_token = response.json().get('access_token')
-    print(f"Access Token: {access_token}")
-
-    # Mapillary access token -- provide your own, replace this example
-    mly_key = access_token
-
-    seq = 'kx7r2uYeFwT6XCfvqh30VZ'
-
-    url = f'https://graph.mapillary.com/image_ids?access_token={mly_key}&sequence_id={seq}'
-
-    response = requests.get(url)
-
-    image_ids = None
-
-    if response.status_code == 200:
-        json = response.json()
-        image_ids = [obj['id'] for obj in json['data']]
-
-        # make a dictionary to store each detection by image ID
-        detections = {}
-        for image_id in image_ids:
-            dets_url = f'https://graph.mapillary.com/{image_id}/detections?access_token={mly_key}&fields=geometry,value'
-            response = requests.get(dets_url)
-            json = response.json()
-            detections[image_id] = json['data']
+    json = response.json()
+    image_ids = [obj['id'] for obj in json['data']]
+    print(image_ids)
