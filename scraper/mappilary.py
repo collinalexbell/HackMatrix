a8c3865aea9870d388815163b1f165a403831f3a
kuberlog <collinalexbell@gmail.com>
2023-11-26 03:05:18 +0000

    getting something back, but can't seem to get it to work right

--- scraper/mappilary.py
+++ scraper/mappilary.py
@@ -12,15 +12,21 @@ access_token = creds.get('access_token')
 # Mapillary access token -- provide your own, replace this example
 mly_key = access_token
 
-seq = 'kx7r2uYeFwT6XCfvqh30VZ'
+image_id = 520273309103543
+minLat = 45.530158055546
+maxLat = 45.530158055576
+minLon = -122.54934638899
+maxLon = -122.54934638887
 
-url = f'https://graph.mapillary.com/image_ids?access_token={mly_key}&sequence_id={seq}'
+#url = f'https://graph.mapillary.com/images?access_token={access_token}&lat=45.530158055556&lng=-122.54934638889&z=17'
+
+url = f'https://graph.mapillary.com/images?access_token={access_token}&bbox={minLon},{minLat},{maxLon},{maxLat}'
 
 response = requests.get(url)
 
-image_ids = None
-
+print(response)
 if response.status_code == 200:
+    print(response.text)
     json = response.json()
-    image_ids = [obj['id'] for obj in json['data']]
-    print(image_ids)
+    print(json)
+
