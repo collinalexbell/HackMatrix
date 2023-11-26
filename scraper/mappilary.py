9a32f2746db4c451181998b61c7347cb85f5039a
kuberlog <collinalexbell@gmail.com>
2023-11-26 08:32:10 +0000

    using the mapillary sdk. Should work!

--- scraper/mappilary.py
+++ scraper/mappilary.py
@@ -1,6 +1,6 @@
 import requests, json
 import yaml
-
+import mapillary.interface as mly
 
 # Load credentials from creds.yaml
 with open('creds.yaml', 'r') as file:
@@ -9,24 +9,8 @@ with open('creds.yaml', 'r') as file:
 # Extract credentials from the loaded data
 access_token = creds.get('access_token')
 
-# Mapillary access token -- provide your own, replace this example
-mly_key = access_token
+mly.set_access_token(access_token)
 
-image_id = 520273309103543
-minLat = 45.530158055546
-maxLat = 45.530158055576
-minLon = -122.54934638899
-maxLon = -122.54934638887
+data = mly.get_image_close_to(latitude=45.530158055556, longitude=-122.54934638889).to_dict()
 
-#url = f'https://graph.mapillary.com/images?access_token={access_token}&lat=45.530158055556&lng=-122.54934638889&z=17'
-
-url = f'https://graph.mapillary.com/images?access_token={access_token}&bbox={minLon},{minLat},{maxLon},{maxLat}'
-
-response = requests.get(url)
-
-print(response)
-if response.status_code == 200:
-    print(response.text)
-    json = response.json()
-    print(json)
-
+print(data)
