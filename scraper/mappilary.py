586dbdab0c4c180d343419468182b9990d22a56e
kuberlog <collinalexbell@gmail.com>
2023-11-26 10:37:18 +0000

    didn't really work too well... but it did something

--- scraper/mappilary.py
+++ scraper/mappilary.py
@@ -11,6 +11,15 @@ access_token = creds.get('access_token')
 
 mly.set_access_token(access_token)
 
-data = mly.get_image_close_to(latitude=45.530158055556, longitude=-122.54934638889).to_dict()
+latitude=45.530158055556
+longitude=-122.54934638889
+sequences = mly.sequences_in_bbox({
+    'west': longitude - 0.0000000001,
+    'east': longitude + 0.0000000001,
+    'north': latitude + 0.0000000001,
+    'south': latitude - 0.0000000001
+})
 
-print(data)
+#data = mly.get_image_close_to(latitude=45.530158055556, longitude=-122.54934638889).to_dict()
+
+print(sequences)
