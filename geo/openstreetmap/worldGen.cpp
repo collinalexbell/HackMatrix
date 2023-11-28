9f48dcf561f588243c736f53c82db75ac7ba3365
kuberlog <collinalexbell@gmail.com>
2023-11-28 05:56:11 +0000

    I've made a hndlr

--- geo/openstreetmap/worldGen.cpp
+++ geo/openstreetmap/worldGen.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <osmium/handler.hpp>
 #include <osmium/io/any_input.hpp>
+#include <osmium/visitor.hpp>
 
 using namespace std;
+
+class MyHandler : public osmium::handler::Handler {
+public:
+  void way(const osmium::Way &way) {
+    std::cout << "way " << way.id() << '\n';
+    for (const osmium::Tag &t : way.tags()) {
+      std::cout << t.key() << "=" << t.value() << '\n';
+    }
+  }
+
+  void node(const osmium::Node &node) {
+    std::cout << "node " << node.id() << '\n';
+  }
+};
+
 int main() {
   cout << "world generator" << endl;
   osmium::io::File input_file{"testMap.osm"};
   osmium::io::Reader reader{input_file};
-  auto buffer = reader.read();
-  osmium::io::Header header = reader.header();
-  for (auto it = buffer.begin(); it != buffer.end(); it++) {
-    cout << it->data() << endl;
-  }
+  MyHandler handler;
+  osmium::apply(reader, handler);
+  reader.close();
 }
