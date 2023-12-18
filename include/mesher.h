7b25477efe05fe583d1ded755048c5e785fa1e96
kuberlog <collinalexbell@gmail.com>
2023-12-18 19:02:44 +0000

    speedups with shared_ptr

--- include/mesher.h
+++ include/mesher.h
@@ -28,8 +28,8 @@ class Mesher {
   int chunkX, chunkZ;
   bool damagedGreedy;
   bool damagedSimple;
-  ChunkMesh cachedGreedyMesh;
-  ChunkMesh cachedSimpleMesh;
+  shared_ptr<ChunkMesh> cachedGreedyMesh;
+  shared_ptr<ChunkMesh> cachedSimpleMesh;
   static glm::vec2 texModels[6][6];
   static Face neighborFaces[6];
   static glm::vec3 faceModels[6][6];
@@ -38,10 +38,13 @@ class Mesher {
   vector<glm::vec3> getOffsetsFromFace(Face face);
   Face getFaceFromNormal(glm::vec3 normal);
 public:
-  Mesher(int chunkX, int chunkZ):chunkX(chunkX), chunkZ(chunkZ){}
+  Mesher(int chunkX, int chunkZ):chunkX(chunkX), chunkZ(chunkZ){
+    cachedGreedyMesh = make_shared<ChunkMesh>(ChunkMesh());
+    cachedSimpleMesh = make_shared<ChunkMesh>(ChunkMesh());
+  }
   ChunkMesh meshedFaceFromPosition(Chunk* chunk, Position position);
-  ChunkMesh meshGreedy(Chunk *chunk);
-  ChunkMesh simpleMesh(Chunk *chunk);
-  ChunkMesh mesh(bool realTime, Chunk* chunk);
+  shared_ptr<ChunkMesh> meshGreedy(Chunk *chunk);
+  shared_ptr<ChunkMesh> simpleMesh(Chunk *chunk);
+  shared_ptr<ChunkMesh> mesh(bool realTime, Chunk* chunk);
   void meshDamaged();
 };
