43cc1d3f2aa767c8abdf1aa2a02b1b652c15a945
kuberlog <collinalexbell@gmail.com>
2024-01-14 08:49:58 +0000

    lol

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -37,6 +37,7 @@ shared_ptr<TexturePack> blocks::initializeBasicPack() {
       Block{161, "acacia leaves", "images/oak.png", true},
       Block{162, "acacia wood", "acacia_log.png"},
       Block{168, "prismarine", "prismarine.png"},
+      Block{172, "hardened clay", "clay.png"},
       Block{173, "block of coal", "coal_block.png"},
       Block{179, "red sandstone", "red_sandstone.png"},
       Block{180, "wooden trapdoor", "birch_trapdoor.png"},
