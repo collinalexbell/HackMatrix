81cd29650f1b6c06ebe0ca0b931fb51432bd9c10
kuberlog <collinalexbell@gmail.com>
2024-02-14 13:28:59 +0000

    I want to mock the domain object

--- test/api.cpp
+++ test/api.cpp
@@ -1,7 +1,12 @@
+#include "blocks.h"
 #include "catch_amalgamated.hpp"
+#include <zmq/zmq.hpp>
 
 #include "api.h"
 
 TEST_CASE("true") {
+  //World* world = mockWorld();
+  auto context = zmq::context_t(2);
+  auto socket = zmq::socket_t(context, zmq::socket_type::req);
   REQUIRE(true);
 }
