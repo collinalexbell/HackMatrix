f49858572261573cfbe5412cc345f1014bf16481
kuberlog <collinalexbell@gmail.com>
2024-02-22 17:18:04 +0000

    fix trampoline

--- src/trampoline.cpp
+++ src/trampoline.cpp
@@ -1,24 +1,18 @@
 #include <cstdlib>
 #include <unistd.h>
 int main(int argc, char** argv, char** envp) {
-  int pid = fork();
-  if(pid == 0) {
-    chdir("/home/collin/cyber-logos");
-    execle("start", "start", NULL, envp);
-    exit(0);
-  }
-  sleep(5);
-  while(true) {
+  int pid ;
+  while (true) {
     int rv = system("/home/collin/matrix/matrix");
-    if(rv != 0) {
+    if (rv != 0) {
       pid = fork();
-      if(pid == 0) {
+      if (pid == 0) {
         sleep(2);
-        system("xdotool search --class Xterm | xargs ./build/x-raise");
+        system("xdotool search --class Terminator | xargs ./build/x-raise");
         exit(0);
       }
-      int exit = system("xterm");
-      if(exit == 42) {
+      int exit = system("terminator");
+      if (exit == 42) {
         break;
       }
     }
