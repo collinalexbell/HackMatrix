922a7e6baba62215afe9e7f56a9eb5cdc426d7a6
kuberlog <collinalexbell@gmail.com>
2024-02-29 21:54:35 +0000

    the backpack loads properly

--- src/model.cpp
+++ src/model.cpp
@@ -125,6 +125,7 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
     glGenTextures(1, &textureID);
 
     int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
     unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
     if (data)
     {
