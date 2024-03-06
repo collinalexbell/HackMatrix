29026dfbed7cd2ac2b79edb2958c56f8ca1ee845
kuberlog <collinalexbell@gmail.com>
2024-03-06 06:37:34 +0000

    Lock and Key structs

--- include/components/Key.h
+++ include/components/Key.h
@@ -1,4 +1,14 @@
 #pragma once
+#include "components/RotateMovement.h"
+#include <entt.hpp>
+
+enum TurnState {
+  TURNED, TURNING, UNTURNED, UNTURINING
+};
 
 struct Key {
+  entt::entity lockable;
+  TurnState state;
+  RotateMovement turnMovement;
+  RotateMovement unturnMovement;
 };
--- include/components/Lock.h
+++ include/components/Lock.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "glm/glm.hpp"
+enum LockState {
+  UNLOCKED, LOCKED
+};
 struct Lock {
-  
+  glm::vec3 position;
+  glm::vec3 tolerance;
+  LockState state;
 };
