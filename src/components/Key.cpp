0b25ca20fc37f84c1d391c66a9a6328aa40d154f
kuberlog <collinalexbell@gmail.com>
2024-03-06 06:50:07 +0000

    stub Lock.cpp and add createTablesIfNeeded for KeyPersister

--- src/components/Key.cpp
+++ src/components/Key.cpp
@@ -1,6 +1,31 @@
 #include "components/Key.h"
 
-void KeyPersister::createTablesIfNeeded(){}
+void KeyPersister::createTablesIfNeeded() {
+  SQLite::Database &db = registry->getDatabase();
+
+  /* This exists in Door.cpp. Should refactor into RotateMovement component
+  db.exec("CREATE TABLE IF NOT EXISTS RotateMovement ( "
+          "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+          "axis_x REAL, "
+          "axis_y REAL, "
+          "axis_z REAL, "
+          "degrees REAL, "
+          "degrees_per_second REAL "
+          ")");
+  */
+
+  db.exec("CREATE TABLE IF NOT EXISTS  ( "
+          "id INTEGER PRIMARY KEY, "
+          "entity_id INTEGER, "
+          "turn_movement_id INTEGER, "
+          "unturn_movement_id INTEGER, "
+          "state INTEGER, "
+          "FOREIGN KEY (entity_id) REFERENCES Entity(id), "
+          "FOREIGN KEY (turn_movement_id) REFERENCES RotateMovement(id), "
+          "FOREIGN KEY (unturn_movement_id) REFERENCES RotateMovement(id) "
+          ")");
+}
+
 void KeyPersister::saveAll(){};
 void KeyPersister::save(entt::entity){};
 void KeyPersister::loadAll(){}
---
+++ src/components/Lock.cpp
@@ -0,0 +1,7 @@
+#include "components/Lock.h"
+
+void LockPersister::createTablesIfNeeded() {}
+void LockPersister::saveAll(){};
+void LockPersister::save(entt::entity){};
+void LockPersister::loadAll() {}
+void LockPersister::load(entt::entity){};
