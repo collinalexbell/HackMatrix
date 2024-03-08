2f562e5f26faf9fa5d334a9b30bcb4f2a042a221
collinalexbell <collinalexbell@gmail.com>
2024-03-08 22:14:44 +0000

    PHOEN-46: make scriptable component (#16)
    * scriptable kind of works
    
    * mv template to SQLPersister. Add ScriptablePersister
    
    * alright, Scriptable is done!

---
+++ include/components/Scriptable.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "SQLPersisterImpl.h"
+#include "string"
+#include <mutex>
+
+enum ScriptLanguage {
+  CPP, JAVASCRIPT, PYTHON
+};
+
+class Scriptable {
+  std::mutex _mutex;
+  std::string script;
+  bool isDamaged;
+ public:
+  Scriptable(std::string script, ScriptLanguage language);
+  std::string getScript();
+  void setScript(std::string);
+  ScriptLanguage language;
+  std::string getExtension();
+  void damage();
+  void update();
+};
+
+class ScriptablePersister: public SQLPersisterImpl {
+ public:
+  ScriptablePersister(std::shared_ptr<EntityRegistry> registry):
+      SQLPersisterImpl("Scriptable", registry){};
+  void createTablesIfNeeded() override;
+  void saveAll() override;
+  void save(entt::entity) override;
+  void loadAll() override;
+  void load(entt::entity) override;
+  void depersistIfGone(entt::entity) override;
+};
+
