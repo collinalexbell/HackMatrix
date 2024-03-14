ac050595e48962a397b655ba64b3e8f510d91083
kuberlog <collinalexbell@gmail.com>
2024-03-14 23:04:16 +0000

    the move API seems to work now

--- client_libs/python/hackMatrix/api.py
+++ client_libs/python/hackMatrix/api.py
@@ -7,13 +7,25 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://127.0.0.1:3333")
 
 def turnKey(entityId, onOrOff):
-    turnKeyMessage = api_pb2.TurnKey(on=onOrOff)
+    commandMessage = api_pb2.TurnKey(on=onOrOff)
     apiRequest = api_pb2.ApiRequest(entityId=entityId,
                                     type="TURN_KEY",
-                                    turnKey=turnKeyMessage)
+                                    turnKey=commandMessage)
     serializedRequest = apiRequest.SerializeToString()
     socket.send(serializedRequest)
     socket.recv()
+
+def move(entityId, xDelta, yDelta, zDelta, unitsPerSecond):
+    commandMessage = api_pb2.Move(xDelta=xDelta, yDelta=yDelta, zDelta=zDelta,
+                                  unitsPerSecond=unitsPerSecond )
+    apiRequest = api_pb2.ApiRequest(entityId=entityId,
+                                    type="MOVE",
+                                    move=commandMessage)
+    serializedRequest = apiRequest.SerializeToString()
+    socket.send(serializedRequest)
+    socket.recv()
+
+
 
 
 if __name__ == "__main__":
