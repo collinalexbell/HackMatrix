37c5e0be265e6e16b2010ad43dc1814e26a6c392
kuberlog <collinalexbell@gmail.com>
2024-03-14 10:36:06 +0000

    components TranslateMovement and system ApplyTranslation

---
+++ src/systems/ApplyTranslation.cpp
@@ -0,0 +1,57 @@
+#include "systems/ApplyTranslation.h"
+#include "components/TranslateMovement.h"
+#include "glm/ext/quaternion_trigonometric.hpp"
+#include "glm/geometric.hpp"
+#include "glm/gtx/transform.hpp"
+#include "model.h"
+#include "components/Parent.h"
+#include <GLFW/glfw3.h>
+#include <glm/gtc/quaternion.hpp>
+
+double MIN_DELTA = 0.0001;
+void systems::applyTranslations(std::shared_ptr<EntityRegistry> registry) {
+  // initialize to current time on first call
+  static double lastTranslated = glfwGetTime();
+
+  double curTime = glfwGetTime();
+
+  auto toRotate = registry->view<Positionable, TranslateMovement>();
+  for (auto [entity, positionable, translateMovement] : toRotate.each()) {
+    auto delta =
+      translateMovement.delta *
+      glm::vec3(translateMovement.unitsPerSecond * (curTime - lastTranslated));
+
+    // Ensure degrees are always positive for 'min' calculation
+    delta = glm::abs(delta);
+    delta = min(delta, glm::abs(translateMovement.delta));
+
+    // Add the signs back in (after taking min)
+    delta *= glm::sign(translateMovement.delta);
+
+    translateMovement.delta -= delta;
+
+    positionable.pos += delta;
+
+    if (glm::length(translateMovement.delta) < MIN_DELTA) { // Account for negatives
+      if(translateMovement.onFinish.has_value()) {
+        (*translateMovement.onFinish)();
+      }
+      registry->remove<TranslateMovement>(entity);
+    }
+    positionable.damage();
+
+    auto parent = registry->try_get<Parent>(entity);
+    if (parent != NULL) {
+      for (auto childId : parent->childrenIds) {
+        auto childEntityOpt = registry->locateEntity(childId);
+        if(childEntityOpt.has_value()) {
+          auto childEntity = childEntityOpt.value();
+          auto &childPositionable = registry->get<Positionable>(childEntity);
+          childPositionable.pos += delta;
+          childPositionable.damage();
+        }
+      }
+    }
+  }
+  lastTranslated = curTime;
+}
