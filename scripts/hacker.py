cb5556d8fffddbc258a377f1b45bfc54f0e070e4
kuberlog <collinalexbell@gmail.com>
2024-03-20 23:43:51 +0000

    the select-ide script is pretty much done (until the API is created)

--- scripts/hacker.py
+++ scripts/hacker.py
@@ -1,10 +1,33 @@
 from rich.console import Console
 from rich.text import Text
+from rich.progress import Progress, SpinnerColumn
 import subprocess
+import time
 
-# Define the path to your Bash script
-bash_script_path = "/path/to/your/script.sh"
+def select_ide(console):
+    pass
 
+def talk(console):
+    text = Text()
+    text.append("Hello! Welcome to ")
+    text.append("HackMatrix", style="bold green")
+    console.print(text)
+
+    progress = Progress(SpinnerColumn())
+    # Start the progress rendering
+    with progress:
+        # Update the progress continuously for 1 second
+        for _ in progress.track(range(100)):
+            time.sleep(0.01)  # Adjust this sleep duration to control the speed of the spinner animation
+
+    text = Text()
+    text.append("You should `select-ide`")
+    console.print(text)
+
+knownCommands = {
+    "talk": talk,
+    "select-ide": select_ide
+}
 
 cmd = ""
 console = Console()
@@ -14,4 +37,7 @@ while cmd != "exit":
     text.stylize("bold magenta", 0, 3)
     text.stylize("bold green", 4, 10)
     cmd = console.input(text)
-    subprocess.run(cmd, shell=True)
+    if cmd in knownCommands:
+        knownCommands[cmd](console)
+    else:
+        subprocess.run(cmd, shell=True)
