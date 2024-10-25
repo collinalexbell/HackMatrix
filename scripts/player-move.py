e6eb16ed1ea000ec8231a34ac0f41c15db245fed
collinalexbell <collinalexbell@gmail.com>
2024-10-25 02:54:14 +0000

    I really like the ending of this one

--- scripts/player-move.py
+++ scripts/player-move.py
@@ -1,3 +1,35 @@
 import hackMatrix.api as matrix
+import time
 
-matrix.player_move((10, 10.5, -20), (0, 180, 0), 2)
+scale = 300 
+time.sleep(1)
+burst_margin_t = 3
+matrix.player_move((scale/5, 0, 0), (0, 180, 0), 7)
+time.sleep(7+burst_margin_t)
+matrix.player_move((0, scale * 0.6, 0), (0, 180, 0), 3)
+time.sleep(10)
+
+swipes = 7 
+for i in range(swipes):
+    mult = 1
+    if i % 2 == 0:
+        mult = -1
+    time_to_move = 2.5
+    matrix.player_move((scale * mult, 0, 0), (0, 23 * mult, 0), time_to_move)
+    time.sleep(time_to_move)
+
+time.sleep(5)
+
+# teleport to a window on the ship
+# super + e
+
+swipes = 30
+for i in  range(swipes):
+    mult = 1
+    if i % 2 == 0:
+        mult = -1
+    time_to_move =0.2 
+    matrix.player_move((scale * 0.01* mult, 0, 0), (0, 23 * mult, 0), time_to_move)
+    time.sleep(time_to_move)
+
+
