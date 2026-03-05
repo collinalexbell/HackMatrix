3e4da70aa8f4fef343bc889b56915166464ee927
collinalexbell <collinalexbell@gmail.com>
2026-03-05 12:28:22 -0500

    add clarifying comments to all of process_key_sym

--- src/wayland/wlr_compositor.cpp
+++ src/wayland/wlr_compositor.cpp
@@ -831,6 +831,9 @@ process_key_sym(WlrServer* server,
                 uint32_t keycode = 0,
                 bool update_mods = false)
 {
+
+  // This is an assertion and generally shouldn't happen
+  // in a properly initialized HackMatrix
   if (!server || !server->engine) {
     return;
   }
@@ -843,16 +846,31 @@ process_key_sym(WlrServer* server,
   bool modifierHeld = (mods & server->hotkeyModifierMask);
   bool shiftHeld = (mods & WLR_MODIFIER_SHIFT);
 
+  // This is where all WM controls are handled (except for QUIT, see below)
   auto resp = controls->handleKeySym(sym, pressed, modifierHeld, shiftHeld);
+
+
+  // QUIT can not be handled inside the controls itself
+  // It must be handled in the serve that owns the display
   if (resp.requestQuit) {
-    // wlr is the owner of the display, so it must handle terminate
     wl_display_terminate(server->display);
     return;
   }
+
+
+  // In the event that WM controls handled a symkey,
+  // it should not be passed to the app,
+  // so return before the seat is notified.
   if (resp.consumed) {
-    // the WM handled the key, do not pass to an app
     return;
   }
+
+  // Send the event created by wlr_keybard to the wlr_seat
+  // wlr_keyboard is a physical device listened to by wlr
+  // In contrast, wlr_seat is a user input context that can merge
+  // multiple physical devices or not recieve input from any devices.
+  // Apps are notified by seats (reigstered happens in WaylandApp.takeInputFocus)
+  // Seats are notified by wlr_keyboard (here, below)
   enum wl_keyboard_key_state state =
     pressed ? WL_KEYBOARD_KEY_STATE_PRESSED : WL_KEYBOARD_KEY_STATE_RELEASED;
   wlr_seat_keyboard_notify_key(server->seat, time_msec, keycode, state);
